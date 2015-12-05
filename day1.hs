97027ae473b9b1a10ebc91793b391d2fcc7ffd7e
Daniel Filan <df@danielfilan.com>
2015-12-06 08:57:04 +1100

    first part of day 1

--- day1.hs
+++ day1.hs
@@ -1,3 +1,15 @@
-listParensToFloor    :: [Char] -> Int
-listParensToFloor []   = 0
-listParensToFloor x:xs = case x of who knows
+import System.IO
+
+listParensToFloor        :: [Char] -> Int
+listParensToFloor []     = 0
+listParensToFloor (x:xs) = case x of
+                            '(' -> 1 + listParensToFloor xs
+                            ')' -> (0-1) + listParensToFloor xs
+                            _   -> listParensToFloor xs
+
+main = do
+  parens_file <- openFile "parens_input.txt" ReadMode
+  listParens  <- hGetContents parens_file
+  let floor = listParensToFloor listParens
+  print floor
+  hClose parens_file
