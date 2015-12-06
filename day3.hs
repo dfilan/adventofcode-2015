594a0d97fb2f757de74fed0ae1ea6cd39ff14134
Daniel Filan <df@danielfilan.com>
2015-12-06 12:37:02 +1100

    first pass at day 3, correct but too slow

---
+++ day3.hs
@@ -0,0 +1,31 @@
+import Data.Complex
+import Data.List
+import System.IO
+
+charToComplex :: (RealFloat a) => Char -> Complex a
+charToComplex char = case char of
+                      '>' -> 1 :+ 0
+                      '^' -> 0 :+ 1
+                      '<' -> (-1) :+ 0
+                      'v' -> 0 :+ (-1)
+                      _   -> 0 :+ 0
+
+numsVisited :: (RealFloat a) => String -> [Complex a]
+numsVisited = map (sum . map charToComplex) . inits
+
+-- super inefficient!
+uniques :: (Eq a) => [a] -> [a]
+uniques [] = []
+uniques (x:xs)
+ | elem x $ uniques xs = uniques xs
+ | otherwise           = x : uniques xs
+
+diffNumsVisited :: String -> Int
+diffNumsVisited = length . uniques . numsVisited
+
+main = do
+    withFile "grid_movements.txt" ReadMode (\handle -> do
+      contents <- hGetContents handle
+      let houses_visited = diffNumsVisited contents
+      putStrLn "part 1:"
+      print houses_visited)
