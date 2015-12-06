887a158bc6c995b11667c5d423e4ae46ddf3c25a
Daniel Filan <df@danielfilan.com>
2015-12-06 12:09:53 +1100

    finished day 2

--- day2.hs
+++ day2.hs
@@ -17,6 +17,18 @@ paperNeeded list = (totalSA list) + (minFace list)
 totalPaper :: (Num a, Ord a) => [[a]] -> a
 totalPaper = sum . map paperNeeded
 
+perims :: (Num a) => [a] -> [a]
+perims =  map (*2) . map sum . filter (\x -> length x == 2) . subsequences
+
+wrapRibbon :: (Num a, Ord a) => [a] -> a
+wrapRibbon = minimum . perims
+
+ribbonNeeded :: (Num a, Ord a) => [a] -> a
+ribbonNeeded dimensions = (product dimensions) + (wrapRibbon dimensions)
+
+totalRibbon :: (Num a, Ord a) => [[a]] -> a
+totalRibbon = sum . map ribbonNeeded
+
 -- this string to int program will only work for strings like "19"
 stringToInt :: String -> Int
 stringToInt = sum . zipWith (*) [10^n | n <- [0..]]
@@ -35,8 +47,15 @@ strToDigits = map stringToInt . groupDigits
 fileToTotalPaper :: String -> Int
 fileToTotalPaper = totalPaper . map strToDigits . lines
 
+fileToTotalRibbon :: String -> Int
+fileToTotalRibbon = totalRibbon . map strToDigits . lines
+
 main = do
   withFile "box_list.txt" ReadMode (\handle -> do
       contents <- hGetContents handle
       let paper = fileToTotalPaper contents
-      print paper)
+      putStrLn "part 1:"
+      print paper
+      let ribbon = fileToTotalRibbon contents
+      putStrLn "part 2:"
+      print ribbon)
